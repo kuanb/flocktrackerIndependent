d5097c03d36e8d7f19e6df719f72073359e62847
buzoherbert <buzoherbert@gmail.com>
2014-08-10 15:11:45 +0700

    added answer model

---
+++ src/org/urbanlaunchpad/flocktracker/models/Answer.java
@@ -0,0 +1,23 @@
+package org.urbanlaunchpad.flocktracker.models;
+
+public class Answer {
+	private String answerText;
+	private String jumpID;
+
+	public String getAnswerText() {
+		return answerText;
+	}
+
+	public void setAnswerText(String answerText) {
+		this.answerText = answerText;
+	}
+	
+	public String getJumpID() {
+		return jumpID;
+	}
+
+	public void setjumpID(String jumpID) {
+		this.answerText = jumpID;
+	}
+
+}
